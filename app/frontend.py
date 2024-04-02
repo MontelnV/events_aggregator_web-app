7f0f4b1970e38d767c47c58076ee283894c3acb0
MontelnV <vsor2005@gmail.com>
2024-04-03 00:42:36 +0500

    create main fr routers

--- app/frontend.py
+++ app/frontend.py
@@ -1,74 +1,63 @@
-from __future__ import annotations as _annotations
-from fastapi import APIRouter, Depends
-from typing import Annotated
-import json
-from fastapi.responses import HTMLResponse
-from app.database import EventsORM
-from app.schemas import Event, EventAdd, Auth
+from fastapi import APIRouter, FastAPI, Request, status
+from typing import Optional
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from app.repositories import EventRepository
-from fastapi import APIRouter
-from fastui import AnyComponent, FastUI, prebuilt_html
-from fastui import components as c
-from fastui.components.display import DisplayLookup, DisplayMode
-from fastui.events import GoToEvent, BackEvent
-from app.database import new_session
+from app.schemas import EventAdd
+from app.database import EventsORM, new_session
 from sqlalchemy import select
-from contextlib import asynccontextmanager
-from datetime import datetime
+from fastapi.responses import RedirectResponse
+
 
 router = APIRouter(
-    tags=["Frontend"],
+    prefix="/events",
+    tags=["Pages"]
 )
-#FAST UI
-@router.get('/api/', response_model=FastUI, response_model_exclude_none=True)
-async def main_page() -> list[AnyComponent]:
-    async with new_session() as session:
-        query = select(EventsORM)
-        result = await session.execute(query)
-        data = result.scalars().all()
-        event_dicts = [{column.name: getattr(event, column.name) for column in EventsORM.__table__.columns}
-                       for event in data]
-        events = [EventAdd(**event_dict) for event_dict in event_dicts]
 
-    return [
-        c.Page(
-            components=[
-                c.Button(text="Личный кабинет администратора", on_click=GoToEvent(url='/admin')),
-                c.PageTitle(text="Главная страница"),
-                c.Heading(text="Агрегатор мероприятий", level=1),
-                c.Table(
-                    data=events,
-                    columns=[
-                        DisplayLookup(title='Название', field="title", table_width_percent=5),
-                        DisplayLookup(title='Описание', field="description", table_width_percent=50),
-                        DisplayLookup(title='Организатор', field="organizer"),
-                        DisplayLookup(title='Дата события', field="event_date", table_width_percent=5),
-                        DisplayLookup(title='Время закрытия регистрации', field="registration_close_datetime"),
-                        DisplayLookup(title='Место проведения', field="location"),
-                        DisplayLookup(title='Ссылка на регистрацию', field="registration_link", table_width_percent=10),
-                    ]
-                ),
-            ]
-        )
-    ]
+templates = Jinja2Templates(directory="templates")
 
-@router.get('/api/admin', response_model=FastUI, response_model_exclude_none=True)
-async def main_page() -> list[AnyComponent]:
-    return [
-        c.Page(
-            components=[
-                c.Button(text="Вернуться назад", on_click=BackEvent()),
-                c.PageTitle(text="ЛК"),
-                c.Heading(text="Вход в личный кабинет", level=1),
-                c.ModelForm(
-                    model=Auth,
-                    submit_url="/api/user"
-                )
+def check_authentication(request: Request):
+    session_token = request.cookies.get("session_token")
+    if session_token is None or session_token != "Dy8HcAVc05afGsbP4wOF6fRJsWR0Wju1b49rljExFoQh0f0f8qZ75lw2ymXPKhux":
+        return False
+    return True
 
-            ]
-        )
-    ]
-#без следующей функции ничего не билдится, важная оч
-@router.get('/{path:path}')
-async def html_landing() -> HTMLResponse:
-    return HTMLResponse(prebuilt_html())
+@router.get("/")
+async def login_page(request: Request, showAll: bool = False):
+    events = await EventRepository.get_events(showAll)
+    return templates.TemplateResponse("index.html", {"request": request, "events": events})
+
+@router.get("/login")
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router.get("/admin")
+async def home(request: Request, showAll: bool = False):
+    if not check_authentication(request):
+        return RedirectResponse(url="/events/login", status_code=status.HTTP_303_SEE_OTHER)
+
+    events = await EventRepository.get_events(showAll)
+    return templates.TemplateResponse("index.html", {"request": request, "events": events})
+
+@router.get("/admin/addnew")
+async def addnew(request: Request):
+    if not check_authentication(request):
+        return RedirectResponse(url="/events/login", status_code=status.HTTP_303_SEE_OTHER)
+
+    return templates.TemplateResponse("addnew.html", {"request": request})
+
+@router.get("/admin/edit/{event_id}")
+async def edit(request: Request, event_id: int):
+    if not check_authentication(request):
+        return RedirectResponse(url="/events/login", status_code=status.HTTP_303_SEE_OTHER)
+
+    event = await EventRepository.get_event_by_id(event_id)
+    return templates.TemplateResponse("edit.html", {"request": request, "event": event})
+
+@router.get("/admin/delete/{event_id}")
+async def delete(request: Request, event_id: int):
+    if not check_authentication(request):
+        return RedirectResponse(url="/events/login", status_code=status.HTTP_303_SEE_OTHER)
+
+    await EventRepository.delete_event(event_id)
+    return RedirectResponse(url="/events/admin?showAll=True", status_code=status.HTTP_303_SEE_OTHER)
