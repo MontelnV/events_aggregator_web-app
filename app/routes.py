6c6329ad54e711304185fd7c582b0fc583a072fa
MontelnV <vsor2005@gmail.com>
2024-04-07 23:18:33 +0500

    upd routes

--- app/routes.py
+++ app/routes.py
@@ -16,13 +16,13 @@ router = APIRouter(
 @router.post("/events/login")
 async def admin_login(username: str = Form(...), password: str = Form(...)):
     if username == "admin" and password == "admin":
-        response = RedirectResponse(url="/events/adminlk", status_code=status.HTTP_302_FOUND)
+        response = RedirectResponse(url="/events/admin", status_code=status.HTTP_302_FOUND)
         response.set_cookie(key="session_token", value="Dy8HcAVc05afGsbP4wOF6fRJsoghkju1b49rljExFoQh0f0f8qZ75lw2ymXPKhux")
         return response
     else:
         return RedirectResponse(url="/events/login", status_code=status.HTTP_303_SEE_OTHER)
 
-@router.get("/events/adminlk/logout")
+@router.get("/events/admin/logout")
 async def admin_logout(request: Request):
     response = RedirectResponse(url="/events/login", status_code=status.HTTP_302_FOUND)
     response.delete_cookie("session_token")  # Удаление сессионного токена из cookies
@@ -38,7 +38,7 @@ async def create_event(request: Request, title: str = Form(...),
                      registration_link=registration_link, description=description)
 
     await EventRepository.add_event(event)
-    return RedirectResponse(url="/events/adminlk?showAll=True", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/events/admin?showAll=True", status_code=status.HTTP_303_SEE_OTHER)
 
 @router.post("/api/events/{event_id}")
 async def update_event(request: Request, event_id: int, title: str = Form(...),
@@ -50,12 +50,14 @@ async def update_event(request: Request, event_id: int, title: str = Form(...),
                      registration_close_datetime=registration_close_datetime, location=location,
                      registration_link=registration_link, description=description)
     await EventRepository.update_event(event_id, event)
-    return RedirectResponse(url="/events/adminlk?showAll=True", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(url="/events/admin?showAll=True", status_code=status.HTTP_303_SEE_OTHER)
 
 @router.delete("/api/events/{event_id}")
 async def delete_event(event_id: int):
     result = await EventRepository.delete_event(event_id)
     return {"message": result}
+
+
 
 
 @router.get("/api/events")
