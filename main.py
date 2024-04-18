751be4e69dfe7c2ea32b489ea80d36f76b44c68e
MontelnV <vsor2005@gmail.com>
2024-04-18 11:01:37 +0500

    include auth router

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.database import create_tables, drop_tables
 from contextlib import asynccontextmanager
 from app.routes import router as event_router
 from app.frontend import router as frontend_router
+from app.auth import router as login_router
 
 
 @asynccontextmanager
@@ -15,4 +16,5 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(event_router)
 app.include_router(frontend_router)
+app.include_router(login_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
